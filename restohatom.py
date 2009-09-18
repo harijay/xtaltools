b005004d2e0907d6b6d0f52a97873740d11a5037
hari <harijay@gmail.com>
2009-09-18 16:31:19 -0400

    Good script : hattopdb to convert shelx hatom fractional coordinates to pdb coordinates, rest are hackish old sripts : need to rewrite

--- restohatom.py
+++ restohatom.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python
 import sys
-import optparse
-useage = "usage: %prog [options] arg1 arg2"
-input_parser = optparse.OptionParser(usage=useage,version="%prog 0.1")
-input_parser.add_option("--infile","-i",dest="filename",help="input res file from Shelx",metavar="*.res")
-(myoptions,args) = input_parser.parse_args()
 
-infile =None
-
-infile = myoptions.filename
-
-try:
-    resfile = open(infile,"r")
-except TypeError,IOError:
-    input_parser.print_help()
-    exit()
+infile = sys.argv[1]
+resfile = open(infile,"read")
 
 for i in range(7):
 	resfile.readline()
