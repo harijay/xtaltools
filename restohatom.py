a0213e1924d1cc58aeb7dfa7a82b8c824cff6844
hari <harijay@gmail.com>
2009-09-22 13:49:49 -0400

    restohatom  modified with arguments -a for atomtype and -r for res or HAT file

--- restohatom.py
+++ restohatom.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python
 import sys
+from optparse import OptionParser
+p = OptionParser()
+p.add_option("--resfile" , "-r" , metavar="[*.res or *.hat file]", dest="resfile")
+p.add_option("--atomtype","-a",metavar="SE",dest="atomtype")
+options,args = p.parse_args()
 
-infile = sys.argv[1]
+if len(sys.argv) <= 1 :
+ p.print_help()
+ exit()
+
+infile = options.resfile
+
 resfile = open(infile,"read")
 
 for i in range(7):
 	resfile.readline()
 import re
+
 space = re.compile("\s+")
+
 def hatomize(line):
 	elems = space.split(line)
 	outline ="ATOM   Se  %s  %s  %s\n" % (elems[2],elems[3],elems[4])
@@ -15,8 +27,9 @@ def hatomize(line):
 import os
 outfile= os.path.splitext(infile)[0] + ".hatom"
 hatomfile = open(outfile,"write")
+inputatom = options.atomtype.upper()
 
-atom = re.compile("SE.*")
+atom = re.compile("%s.*" % inputatom)
 resfile.seek(0)
 for line in resfile:
 	print line
