62fee4e5b5357ea8c142464c9baac4e5640b2b97
Hari Jayaram <harijay@gmail.com>
2011-04-26 11:06:08 -0400

    Significant bug in decrunch ..every alternate sequence was ignored-Now fixed

--- decrunch.py
+++ decrunch.py
@@ -2,35 +2,34 @@
 import optparse
 import sys
 import re
+isheader = re.compile("^\>.*")
+found_sequence = "No"
 
-def decrunch_alignment(file):
-	isheader = re.compile("^\>.*")
-	for line in file:
-		if isheader.match(line):
-			print line,
-			out = ""
-			line = file.next()
-			while not isheader.match(line):
-				try:
-					out = out + line.replace(".","")
-					out = out.replace("-","")
-					out = out.replace("\n","")
-					line = file.next()
-				except StopIteration as e:
-					break
-			print out
+def decrunch_alignment(afile):
+    out = ""
+    while 1 :
+        try:
+            line = afile.next()
+            if isheader.match(line):
+                if out != "":
+                    print out
+                print line,
+                found_sequence="Yes"
+                out = ""
+            if ">" not in line and found_sequence=="Yes":
+                out = out + line.replace(".","")
+                out = out.replace("-","")
+                out = out.replace("\n","")
+        except StopIteration as e:
+            print out
+            break
+			
 			
 if __name__ == '__main__':
-	try:
-		p = optparse.OptionParser(" %prog [options]. \nThis program de-aligns Fasta formatted multiple sequence alignments for input into another alignment program")
-		p.add_option("-i", "--infile", dest="infile",metavar="[*.txt infile]")
-		options,args = p.parse_args()
-		decrunch_alignment(open(options.infile))
-	except IndexError as e:
-		p.print_help()
-	except TypeError as e:
-		p.print_help()
-	except NameError as e:
-		p.print_help()
+    p = optparse.OptionParser(" %prog [options]. \nThis program de-aligns Fasta formatted multiple sequence alignments for input into another alignment program")
+    p.add_option("-i", "--infile", dest="infile",metavar="[*.txt infile]")
+    options,args = p.parse_args()
+    decrunch_alignment(open(options.infile))
+	
 		
 		
